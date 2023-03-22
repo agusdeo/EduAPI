48f56800e0244a2c736a8ec5234b24aa3380f523
Agus Deo Saputra Utama <81843382+agusdeo@users.noreply.github.com>
2023-03-22 14:15:59 +0700

    menambahkan tugas Validate Headers

---
+++ pokeAPI/headers.cy.js
@@ -0,0 +1,8 @@
+/// <reference types="cypress" />
+
+describe('Testing API Headers', () => {
+	it('Succesfully Validate body using name parameter ', () => {
+		cy.request('https://pokeapi.co/api/v2/pokemon/ditto').as('pokemon')
+		cy.get('@pokemon').its('body').its('name').should('include', 'ditto')
+	})
+})
